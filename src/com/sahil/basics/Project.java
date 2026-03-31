141fe0528f28c7f6a3bfc97a06de6d5a32a4e0cf
sahilrai0201 <sahilatwork21@gmail.com>
2026-04-01 00:21:34 +0530

    Created a project

--- src/com/sahil/basics/Project.java
+++ src/com/sahil/basics/Project.java
@@ -1,5 +1,32 @@
 package com.sahil.basics;
+import java.util.*;
 
 public class Project {
+    public static void main(String[] args){
+        //MINI PROJECT
+        Scanner sc = new Scanner(System.in);
+        int myNumber = (int)(Math.random()*100);
 
+        int userNumber = 0;
+
+        do{
+            System.out.println("Guess my number(1-100) : ");
+            userNumber = sc.nextInt();
+
+            if(userNumber == myNumber){
+                System.out.println("WOOHOO..Correct Number");
+                break;
+            }
+            else if(userNumber > myNumber){
+                System.out.println("your number is large");
+            }
+            else{
+                System.out.println("your number is small");
+            }
+        }
+        while(userNumber >= 0);
+
+        System.out.print("My number was : ");
+        System.out.println(myNumber);
+    }
 }
